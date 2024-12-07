79d8f828409afb2d1b2a6245ce15e244edab04e4
Gaku Takahashi <gakut2002@gmail.com>
2024-12-07 09:55:00 +0900

    基本画面を作成した

--- LacrosseRule/ContentView.swift
+++ LacrosseRule/ContentView.swift
@@ -9,13 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text("Lax Learner")
+                    .font(.system(size: 60))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .foregroundColor(.white)
+                    .shadow(color: .red, radius: 5, x: 2, y: 2)
+                NavigationLink(destination: MainView()){
+                    Text("スタート")
+                        .font(.title)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct MainView: View{
+    var body: some View{
+        VStack{
+            NavigationLink(destination: TestModeView()){
+                Text("テストモード")
+                    .font(.title)
+                    .frame(width: 250, height:100)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+            NavigationLink(destination: StudyModeView()){
+                Text("復習モード")
+                    .font(.title)
+                    .frame(width: 250, height:100)
+                    .padding()
+                    .background(Color.yellow)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+            NavigationLink(destination: StudyModeView()){
+                Text("試合モード")
+                    .font(.title)
+                    .frame(width: 250, height:100)
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .opacity(0.5)
+            }
+            .disabled(true)
+        }
+    }
+}
+
+struct TestModeView: View{
+    var body: some View{
+        Text("モードを選んでください")
+            .font(.largeTitle)
+    }
+}
+
+struct StudyModeView: View{
+    var body: some View{
+        Text("出題範囲を選んでください")
+            .font(.largeTitle)
     }
 }
 
